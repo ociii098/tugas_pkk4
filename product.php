910122fca28aad49c1c2e4d2af6d5d3448162abf
ociii098 <114707375+ociii098@users.noreply.github.com>
2022-09-30 06:31:50 +0700

    upload file produk

---
+++ product.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+    
+</head>
+<body>
+
+    <nav>
+    
+    <div class="logo">
+       <h1>pkk4tas</h1>
+    </div>
+
+<ul>
+    <li><a href="projectpkk.php">home</a></il>
+    <li><a href="">product</a></il>
+    <li><a href="">info</a></il>
+    <li><a href="">contact us</a></il> 
+</nav>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Pacifico&display=swap" rel="stylesheet">
+<br>
+
+<center>
+<table border="1px"class="euy" width="70%" height="350px">
+<tr>
+    <td><img src="usus.jpeg" width="160px">
+    <br><p> nama produk :Usus</p>
+    <p>harga:2.000</p>
+    <p> usus adalah temuan terbaru yang terbuat dari bahan baku usus ayam pilihan yang berkualitas </p>
+    
+      <button><a href="https://instagram.com/bayyhun_/ighid=YmMyMTA2M2Y=">PESAN</button>
+</table>
+<br>
+<table border="1px"class="euy" width="70%" height="440px">
+
+<tr>
+    <td><img src="cimol.jpeg" width="160px">
+    <br><p> nama produk :cimol</p>
+    <p>harga:2.000</p>
+    <p>Deskripsi: cimol adalah makanan yang Teksturnya lebih kenyal dan basah dibanding cimol pada umumnya. 
+        Dicampur dengan bumbu bawang putih dan cabai bubuk yang membuat rasanya semakin nikmat </p>
+    
+      <button>PESAN</button>
+      
+</table>
+</td>
+<br>
+<table border="1px"class="euy" width="70%" height="500px">
+
+<tr>
+    <td><img src="makaroni.jpeg" width="160px">
+    <br><p> nama produk :makaroni</p>
+    <p>harga:2.000</p>
+    <p>Deskripsi:Merupakan camilan yang terbuat dari pasta kering jenis makaroni dengan perpaduan bumbu daun jeruk dan cabai pedas </p>
+    
+      <button>PESAN</button>
+    
+</table>
+</center>
