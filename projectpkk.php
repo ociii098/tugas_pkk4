23896ec86ff1a8a8cca8cbac99677455ac9c65ce
ociii098 <114707375+ociii098@users.noreply.github.com>
2022-09-30 06:30:56 +0700

    upload file tugas pkk

---
+++ projectpkk.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+    
+</head>
+<body>
+
+    <nav>
+    
+    <div class="logo">
+       <h1>pkk4tas</h1>
+    </div>
+
+<ul>
+    <li><a href="projectpkk.php">home</a></il>
+    <li><a href="product.php">product</a></il>
+    <li><a href="">info</a></il>
+    <li><a href="">contact us</a></il> 
+</nav>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Pacifico&display=swap" rel="stylesheet">
+<table border="1px"class="cok">
+    <h1>anggota</h1><br>
+    <tr>
+        <th><img src="zea.jpeg" width="200" ></th>
+        <th> <li>nama  : zea zalgya <br>
+            <li>alamat : cimek <br>
+            <li>ttl    : 24 mei 2005<br>
+            <li>no.hp  : 085795106379
+        </th>
+    </tr>
+    <tr>
+        <th><img src="akbar.jpeg" width="200" ></th>
+        <th><li>nama   : akbar syam <br>
+            <li>alamat : lembur warung <br>
+            <li> ttl   : 10 januari 2006<br>
+            <li>no.hp  : 085785958404
+        </th>
+    </tr>
+    <tr>
+        <th><img src="rosi.jpeg" width="200" ></th>
+        <th><li>nama   : rosi nopitasari <br>
+            <li>alamat : karang resik <br>
+            <li>ttl    : 25 juli 2006<br>
+            <li>no.hp  : 089636409030
+        </th>
+    </tr>
+
+</table>
+    
+</body>
+</html>
